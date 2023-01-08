89217a985faf63c3aae5f8308f1df39796f95bb8
TArch64 <tarasturchenko1551@gmail.com>
2023-01-08 15:14:06 +0200

    set latest instead of specific version

--- ci/staging/build/microservice.sh
+++ ci/staging/build/microservice.sh
@@ -14,8 +14,8 @@ docker build \
   --tag "$APP_IMAGE/$APP_NAME-builder:latest" \
   --tag "$APP_IMAGE/$APP_NAME-builder:$APP_VERSION" \
   --file "ci/$ENV/build/microservice.Dockerfile" \
-  --cache-from "$APP_IMAGE/app-node:$APP_VERSION" \
-  --cache-from "$APP_IMAGE/app-sources:$APP_VERSION" \
+  --cache-from "$APP_IMAGE/app-node:latest" \
+  --cache-from "$APP_IMAGE/app-sources:latest" \
   --cache-from "$APP_IMAGE/$APP_NAME-builder:latest" \
   --build-arg BUILDKIT_INLINE_CACHE \
   --build-arg CR_LABEL \
@@ -29,8 +29,8 @@ docker build \
   --tag "$APP_IMAGE/$APP_NAME:latest" \
   --tag "$APP_IMAGE/$APP_NAME:$APP_VERSION" \
   --file "ci/$ENV/build/microservice.Dockerfile" \
-  --cache-from "$APP_IMAGE/app-node:$APP_VERSION" \
-  --cache-from "$APP_IMAGE/app-sources:$APP_VERSION" \
+  --cache-from "$APP_IMAGE/app-node:latest" \
+  --cache-from "$APP_IMAGE/app-sources:latest" \
   --cache-from "$APP_IMAGE/$APP_NAME:latest" \
   --cache-from "$APP_IMAGE/$APP_NAME-builder:latest" \
   --build-arg BUILDKIT_INLINE_CACHE \
