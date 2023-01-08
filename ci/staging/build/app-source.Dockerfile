2996ec279cf9c3b3704d7c79eb590d834b389921
TArch64 <tarasturchenko1551@gmail.com>
2023-01-08 14:16:16 +0200

    expose builder image

--- ci/staging/build/app-source.Dockerfile
+++ ci/staging/build/app-source.Dockerfile
@@ -1,6 +1,6 @@
 ARG APP_IMAGE
 ARG APP_VERSION
-FROM ${APP_IMAGE}/app-node:${APP_VERSION}
+FROM $APP_IMAGE/app-node:$APP_VERSION
 
 ARG CR_LABEL
 LABEL org.opencontainers.image.source $CR_LABEL
--- ci/staging/build/microservice.sh
+++ ci/staging/build/microservice.sh
@@ -5,16 +5,32 @@ APP_IMAGE="$1";
 docker pull "$APP_IMAGE/$APP_NAME:latest" || true;
 
 docker build \
-  --tag "$APP_IMAGE/$APP_NAME:latest" \
-  --tag "$APP_IMAGE/$APP_NAME:$APP_VERSION" \
+  --target builder \
+  --tag "$APP_IMAGE/$APP_NAME-builder:latest" \
+  --tag "$APP_IMAGE/$APP_NAME-builder:$APP_VERSION" \
   --file "ci/$ENV/build/microservice.Dockerfile" \
   --cache-from "$APP_IMAGE/app-node:$APP_VERSION" \
   --cache-from "$APP_IMAGE/app-sources:$APP_VERSION" \
-  --cache-from "$APP_IMAGE/$APP_NAME:latest" \
+  --cache-from "$APP_IMAGE/$APP_NAME-builder:latest" \
   --build-arg BUILDKIT_INLINE_CACHE \
   --build-arg CR_LABEL \
   --build-arg APP_NAME \
   --build-arg APP_IMAGE \
   --build-arg APP_VERSION \
   . && \
+docker build \
+  --tag "$APP_IMAGE/$APP_NAME:latest" \
+  --tag "$APP_IMAGE/$APP_NAME:$APP_VERSION" \
+  --file "ci/$ENV/build/microservice.Dockerfile" \
+  --cache-from "$APP_IMAGE/app-node:$APP_VERSION" \
+  --cache-from "$APP_IMAGE/app-sources:$APP_VERSION" \
+  --cache-from "$APP_IMAGE/$APP_NAME:latest" \
+  --cache-from "$APP_IMAGE/$APP_NAME-builder:latest" \
+  --build-arg BUILDKIT_INLINE_CACHE \
+  --build-arg CR_LABEL \
+  --build-arg APP_NAME \
+  --build-arg APP_IMAGE \
+  --build-arg APP_VERSION \
+  . && \
+docker push -a "$APP_IMAGE/$APP_NAME-builder" && \
 docker push -a "$APP_IMAGE/$APP_NAME";
